8fe41405adef244c63bdb16f978d8ac639add50c
Trexter <sheridak@purdue.edu>
2018-04-05 12:21:34 -0400

    first commit on this branch. Starting the CPP.

---
+++ src/state_estimation_node.cpp
@@ -0,0 +1,37 @@
+/*
+ * Mission 7 State Estimation Node
+ */
+
+#include <ros/ros.h>
+
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp> // OpenCV window I/O
+#include <opencv2/imgproc.hpp> // OpenCV image transformations
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
+#include <opencv2/highgui/highgui_c.h>
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/message_filter.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+#include <tf/tf.h>
+#include <tf/tfMessage.h>
+
+#include <image_transport/image_transport.h>
+#include "std_msgs/String.h"
+#include "sensor_msgs/Image.h"
+#include <sstream>
+#include <cv_bridge/cv_bridge.h>
+
+
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "dipa_test");
+
+  
+
+	return 0;
+}
