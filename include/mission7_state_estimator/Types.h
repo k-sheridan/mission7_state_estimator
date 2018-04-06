e3c1758e1a10d77c208304dc27ce11422f3aee2a
Trexter <sheridak@purdue.edu>
2018-04-06 15:18:56 -0400

    added row and column delete functions

--- include/mission7_state_estimator/Types.h
+++ include/mission7_state_estimator/Types.h
@@ -5,6 +5,28 @@
 
 //NOTE: the standard floating point precision we are using is double
 
+void removeRow(Eigen::MatrixXd& matrix, unsigned int rowToRemove)
+{
+    unsigned int numRows = matrix.rows()-1;
+    unsigned int numCols = matrix.cols();
+
+    if( rowToRemove < numRows )
+        matrix.block(rowToRemove,0,numRows-rowToRemove,numCols) = matrix.block(rowToRemove+1,0,numRows-rowToRemove,numCols);
+
+    matrix.conservativeResize(numRows,numCols);
+}
+
+void removeColumn(Eigen::MatrixXd& matrix, unsigned int colToRemove)
+{
+    unsigned int numRows = matrix.rows();
+    unsigned int numCols = matrix.cols()-1;
+
+    if( colToRemove < numCols )
+        matrix.block(0,colToRemove,numRows,numCols-colToRemove) = matrix.block(0,colToRemove+1,numRows,numCols-colToRemove);
+
+    matrix.conservativeResize(numRows,numCols);
+}
+
 
 // x, y, z, thx, thy, thz, dx, dy, dz, wx, wy, wz, ax, ay, az
 #define QUAD_STATE_SIZE 15
@@ -158,7 +180,6 @@ public:
 
     // set the targets uncertainty
     this->Sigma.block<TARGET_STATE_SIZE, TARGET_STATE_SIZE>(old_dim, old_dim) = x.cov;
-
   }
 
 };
