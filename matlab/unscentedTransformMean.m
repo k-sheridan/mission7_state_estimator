7da503ad7ac56f3e51f1cdb6019a1af0619d4329
Mitchell Lee <mnsupreme@gmail.com>
2018-04-06 23:24:07 -0400

    needs debugging

--- matlab/unscentedTransformMean.m
+++ matlab/unscentedTransformMean.m
@@ -2,9 +2,9 @@ function [meanVector] = unscentedTransformMean(weights,sigmaPoints)
     initWeightsMean = weights(1);
     meanVector = initWeightsMean*objecttoVector(sigmaPoints(1));
     for j=2:length(sigmaPoints)
-        meanVector= meanVector + weights(3) * objecttoVector(sigmaPoints(j));
-        meanVecSize = size(meanVector)
         pointSize =size(objecttoVector(sigmaPoints(j)))
+        meanVector= meanVector + weights(3)*objecttoVector(sigmaPoints(j));
+        
     end
 end
 
